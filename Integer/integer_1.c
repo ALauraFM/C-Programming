abcb41223422c0afb213736aeb8097def7dab03c
ALauraFM <analaurafmendonca@gmail.com>
2023-08-30 10:46:07 -0300

    Integer

---
+++ Integer/integer_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("%d", sizeof(int)); //print the size of the integer type(4), using the unary oprator "sizeof"
+    printf("%d", sizeof(short int));
+    return 0;
+}
